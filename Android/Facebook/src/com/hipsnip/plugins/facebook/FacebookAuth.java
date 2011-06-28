951a8e378a570c87e1669a1d4c23f84ad425ad15
Chris Petersen <geek@ex-nerd.com>
2011-06-28 15:25:13 -0700

    whitespace cleanup and tabify java, untabify javascript (by what seemed appropriate based on file contents)

--- Android/Facebook/src/com/hipsnip/plugins/facebook/FacebookAuth.java
+++ Android/Facebook/src/com/hipsnip/plugins/facebook/FacebookAuth.java
@@ -26,12 +26,13 @@ import com.facebook.android.FacebookError;
 import com.facebook.android.Util;
 
 public class FacebookAuth extends Plugin {
-	
+
 	private Facebook mFb;
-    public String callback;
+	public String callback;
+
 	/**
 	 * Executes the request and returns PluginResult.
-	 * 
+	 *
 	 * @param action 		The action to execute.
 	 * @param args 			JSONArry of arguments for the plugin.
 	 * @param callbackId	The callback id used when calling back into JavaScript.
@@ -40,16 +41,16 @@ public class FacebookAuth extends Plugin {
 	public PluginResult execute(String action, JSONArray args, String callbackId) {
 		callback = callbackId;
 		System.out.println("execute: "+ action);
-		
+
 		String first;
-		
+
 		try {
 			first = args.getString(0);
-        } catch (JSONException e) {
+		} catch (JSONException e) {
 			first = "";
-		    Log.w("Facebook-Plugin", "No arguments in execute");
+			Log.w("Facebook-Plugin", "No arguments in execute");
 		}
-		
+
 		if (action.equals("authorize")) {
 			this.authorize(first); // first arg is APP_ID
 		} else if (action.equals("request")){
@@ -57,12 +58,12 @@ public class FacebookAuth extends Plugin {
 		} else if (action.equals("getAccess")){
 			this.getAccess();
 		}
-		
+
 		PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
 		r.setKeepCallback(true);
 		return r;
 	}
-	
+
 	public void getAccess(){
 		JSONObject json = new JSONObject();
 		try {
@@ -70,100 +71,100 @@ public class FacebookAuth extends Plugin {
 			json.put("expires", this.mFb.getAccessExpires());
 		} catch (JSONException e) {
 		}
-	    
+
 		this.success(new PluginResult(PluginResult.Status.OK, json), this.callback);
 	}
-	
+
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		this.mFb.authorizeCallback(requestCode, resultCode, intent);
-        this.getResponse("me");
+		this.getResponse("me");
 	}
-	
+
 	public void getResponse(final String path){
 		Log.d("DroidGap", "onActivityResult FacebookAuth");
 		try {
 			String response = this.mFb.request(path);
 			JSONObject json = Util.parseJson(response);
-            
+
 			this.success(new PluginResult(PluginResult.Status.OK, json), this.callback);
-			
+
 		} catch (java.net.MalformedURLException e) {
 			this.error(new PluginResult(PluginResult.Status.ERROR), this.callback);
 		} catch (java.io.IOException e) {
 			this.error(new PluginResult(PluginResult.Status.ERROR), this.callback);
-        } catch (JSONException e) {
-            Log.w("Facebook-Example", "JSON Error in response");
-        } catch (FacebookError e) {
-            Log.w("Facebook-Example", "Facebook Error: " + e.getMessage());
-        }
+		} catch (JSONException e) {
+			Log.w("Facebook-Example", "JSON Error in response");
+		} catch (FacebookError e) {
+			Log.w("Facebook-Example", "Facebook Error: " + e.getMessage());
+		}
 	}
 	/**
 	 * Identifies if action to be executed returns a value and should be run synchronously.
-	 * 
+	 *
 	 * @param action	The action to execute
 	 * @return			T=returns value
 	 */
 	public boolean isSynch(String action) {
 		return false;
 	}
-    
-    /**
-     * Called by AccelBroker when listener is to be shut down.
-     * Stop listener.
-     */
-    public void onDestroy() { 	
-    }
 
-    //--------------------------------------------------------------------------
-    // LOCAL METHODS
-    //--------------------------------------------------------------------------
-    
-    /**
-     * Display a new browser with the specified URL.
-     * 
-     * @return				"" if ok, or error message.
-     */
-    public void authorize(final String appid) {
-    	Log.d("PhoneGapLog", "authorize");
+	/**
+	 * Called by AccelBroker when listener is to be shut down.
+	 * Stop listener.
+	 */
+	public void onDestroy() {
+	}
+
+	//--------------------------------------------------------------------------
+	// LOCAL METHODS
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Display a new browser with the specified URL.
+	 *
+	 * @return				"" if ok, or error message.
+	 */
+	public void authorize(final String appid) {
+		Log.d("PhoneGapLog", "authorize");
 		final FacebookAuth fba = this;
 		Runnable runnable = new Runnable() {
 			public void run() {
 				fba.mFb = new Facebook(appid);
 				fba.mFb.setPlugin(fba);
-		        fba.mFb.authorize((Activity) fba.ctx, new String[] {}, new AuthorizeListener(fba));
+				fba.mFb.authorize((Activity) fba.ctx, new String[] {}, new AuthorizeListener(fba));
 
 			};
 		};
 		this.ctx.runOnUiThread(runnable);
 
-    }
+	}
 
 	class AuthorizeListener implements DialogListener {
 		final FacebookAuth fba;
-		
+
 		public AuthorizeListener(FacebookAuth fba){
 			super();
 			this.fba = fba;
 		}
-		
+
 		public void onComplete(Bundle values) {
 			//  Handle a successful login
-	   
+
 			Log.d("PhoneGapLog",values.toString());
 			this.fba.getResponse("me");
 		}
-		
+
 		public void onFacebookError(FacebookError e) {
 			e.printStackTrace();
 		}
 
 		public void onError(DialogError e) {
-			e.printStackTrace();        
+			e.printStackTrace();
 		}
 
-		public void onCancel() {        
+		public void onCancel() {
 		}
 	}
 
-    
+
 }
