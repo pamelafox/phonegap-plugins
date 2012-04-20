bf86fe798fb36baf403c7129d32a043bef31bd20
Pamela Fox <pamela.fox@gmail.com>
2012-04-19 17:34:40 -0700

    (Purely a cosmetic change). Simplified the plugin code to directly call the callback functions. Also re-formatted a bit. Only using hasExtra/getExtra myself, I did not test the others.

--- Android/WebIntent/webintent.js
+++ Android/WebIntent/webintent.js
@@ -1,59 +1,39 @@
 /**
- * Phonegap Web Intent plugin
- * Copyright (c) Boris Smus 2010
- *
- */
+ *  * Phonegap Web Intent plugin
+ *   * Copyright (c) Boris Smus 2010
+ *    *
+ *     */
 var WebIntent = function() { 
 
 };
 
-WebIntent.ACTION_SEND = "android.intent.action.SEND";
-WebIntent.ACTION_VIEW= "android.intent.action.VIEW";
-WebIntent.EXTRA_TEXT = "android.intent.extra.TEXT";
+WebIntent.ACTION_SEND   = "android.intent.action.SEND";
+WebIntent.ACTION_VIEW   = "android.intent.action.VIEW";
+WebIntent.EXTRA_TEXT    = "android.intent.extra.TEXT";
 WebIntent.EXTRA_SUBJECT = "android.intent.extra.SUBJECT";
-WebIntent.EXTRA_STREAM = "android.intent.extra.STREAM";
-WebIntent.EXTRA_EMAIL = "android.intent.extra.EMAIL";
+WebIntent.EXTRA_STREAM  = "android.intent.extra.STREAM";
+WebIntent.EXTRA_EMAIL   = "android.intent.extra.EMAIL";
 
 WebIntent.prototype.startActivity = function(params, success, fail) {
-	return PhoneGap.exec(function(args) {
-        success(args);
-    }, function(args) {
-        fail(args);
-    }, 'WebIntent', 'startActivity', [params]);
+    return PhoneGap.exec(success, fail, 'WebIntent', 'startActivity', [params]);
 };
 
 WebIntent.prototype.hasExtra = function(params, success, fail) {
-	return PhoneGap.exec(function(args) {
-        success(args);
-    }, function(args) {
-        fail(args);
-    }, 'WebIntent', 'hasExtra', [params]);
-};
-
-WebIntent.prototype.getUri = function(success, fail) {
-	return PhoneGap.exec(function(args) {
-        success(args);
-    }, function(args) {
-        fail(args);
-    }, 'WebIntent', 'getUri', []);
+    return PhoneGap.exec(success, fail, 'WebIntent', 'hasExtra', [params]);
 };
 
 WebIntent.prototype.getExtra = function(params, success, fail) {
-	return PhoneGap.exec(function(args) {
-        success(args);
-    }, function(args) {
-        fail(args);
-    }, 'WebIntent', 'getExtra', [params]);
+    return PhoneGap.exec(success, fail, 'WebIntent', 'getExtra', [params]);
 };
 
+WebIntent.prototype.getUri = function(success, fail) {
+    return PhoneGap.exec(success, fail, 'WebIntent', 'getUri', []);
+};
 
 WebIntent.prototype.onNewIntent = function(callback) {
-	return PhoneGap.exec(function(args) {
-		callback(args);
-    }, function(args) {
-    }, 'WebIntent', 'onNewIntent', []);
+    return PhoneGap.exec(callback, function(){}, 'WebIntent', 'onNewIntent', []);
 };
 
 PhoneGap.addConstructor(function() {
-	PhoneGap.addPlugin('webintent', new WebIntent());
+    PhoneGap.addPlugin('webintent', new WebIntent());
 });
